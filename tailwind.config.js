80a1453f14b9c41b4338cb583395f7a1db929912
Fortune Samson <mr4chun4u@gmail.com>
2024-05-04 21:05:53 +0100

    Major changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
+  screens: {
+    sm: '468px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkBlue: 'hsl(230, 29%, 20%)',
+        darkGrayishBlue: 'hsl(230, 11%, 40%)',
+        grayishBlue: 'hsl(231, 7%, 65%)',
+        lightGrayishBlue: 'hsl(207, 33%, 95%)',
+        Red: 'hsl(0, 100%, 68%)',
+      },
+      fontFamily: {
+        sans: ['Barlow Condensed', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
